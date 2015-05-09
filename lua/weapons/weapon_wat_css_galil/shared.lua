284f23499a3d260bbb1312e15e9b433d3c0cbc2d
Awcmon <adcarbine@yahoo.com>
2015-05-09 16:25:30 -0700

    Added M249 and Galil
    Buffed MachPist, nerfed Nova.

---
+++ lua/weapons/weapon_wat_css_galil/shared.lua
@@ -0,0 +1,106 @@
+
+AddCSLuaFile( "shared.lua" )
+
+---------
+SWEP.PrintName 					= "Galil"
+SWEP.Category 					= "Wattle CS:S"
+SWEP.Base 						= "weapon_wattlebase_bullet"
+SWEP.Spawnable 					= true
+SWEP.AdminOnly 					= false
+SWEP.m_WeaponDeploySpeed 		= 1
+
+SWEP.Author 					= "Awcmon"
+SWEP.Contact 					= ""
+SWEP.Purpose 					= ""
+SWEP.Instructions 				= ""
+
+SWEP.ViewModel					= "models/weapons/cstrike/c_rif_galil.mdl"
+SWEP.WorldModel					= "models/weapons/w_rif_galil.mdl"
+SWEP.ViewModelFlip 				= false
+SWEP.ViewModelFOV 				= 57
+
+SWEP.Weight 					= 5
+SWEP.AutoSwitchFrom 			= false
+SWEP.AutoSwitchTo 				= false
+
+SWEP.BobScale 					= 0
+SWEP.SwayScale 					= 0
+SWEP.BounceWeaponIcon 			= true
+SWEP.DrawWeaponInfoBox 			= true
+SWEP.DrawAmmo 					= true
+SWEP.DrawCrosshair 				= false
+//SWEP.RenderGroup 				= 
+SWEP.Slot 						= 1
+SWEP.SlotPos 					= 1
+//SWEP.WepSelection 				= 
+SWEP.CSMuzzleFlashes 			= true
+SWEP.CSMuzzleX 					= false
+
+SWEP.Primary.ClipSize			= 35
+SWEP.Primary.DefaultClip 		= 140
+SWEP.Primary.Ammo 				= "smg1"
+SWEP.Primary.Automatic 			= true
+
+SWEP.Secondary.ClipSize 		= -1
+SWEP.Secondary.DefaultClip 		= -1
+SWEP.Secondary.Ammo 			= "none"
+SWEP.Secondary.Automatic 		= false
+
+SWEP.UseHands 					= true
+SWEP.AccurateCrosshair 			= false
+---------
+SWEP.HoldType 					= "ar2"
+
+SWEP.Primary.Damage 			= 40
+SWEP.Primary.DamageFalloff		= 0.001
+SWEP.Primary.Sound				= Sound("Weapon_Galil.Single")
+SWEP.Primary.NumShots			= 1
+SWEP.Primary.Delay				= 60/750
+SWEP.Primary.Cone				= 0.0015
+SWEP.Primary.ClumpCone			= 0
+SWEP.Primary.Tracer				= 0
+SWEP.Primary.TracerName			= "Tracer"
+SWEP.Primary.MuzzleEffects		= { "effect_wat_muzzle_flash", "effect_wat_muzzle_smoke", "effect_wat_muzzle_sparks" }
+
+SWEP.RecoilPitchAdd 			= 0.8
+SWEP.RecoilPitchMul 			= 0.1
+SWEP.RecoilYawAdd 				= 1
+SWEP.RecoilYawMul 				= 0.025
+
+SWEP.SpreadConeAdd 				= 0.003
+SWEP.SpreadRecoveryTime 		= 0.8
+SWEP.SpreadConeAddCrouch 		= 0.0015
+SWEP.SpreadRecoveryTimeCrouch 	= 0.2
+
+SWEP.SpreadModVel 				= 0.085
+SWEP.SpreadModVelMax 			= 0.085
+SWEP.SpreadModInAir				= 0.075
+SWEP.SpreadModCrouch 			= 0.0002
+
+SWEP.ReloadClipInTime			= 1.5
+
+SWEP.VMPosOffset 				= Vector(0,0,0)
+SWEP.VMAngOffset				= Angle(0,0,0)
+SWEP.SprintPos	 				= { Vector(-0.5,-2.5,-3), Vector(-0.5,-2.5,-3), Vector(-0.5,-2.5,-3) }
+SWEP.SprintAng					= { Angle(-12,32,-35), Angle(-6,35,-35), Angle(-12,38,-35) }
+SWEP.WalkPos 					= { Vector(0.75-1,0,-0.5), Vector(1-1,0,0.5), Vector(1.25-1,0,-0.5) }
+SWEP.WalkAng					= { Angle(0,0,0), Angle(0,0,0), Angle(0,0,0) }
+SWEP.InspectPos 				= { Vector(12.229, -7.091, -5.801), Vector(0.36, -4.553, -11.716) }
+SWEP.InspectAng 				= { Angle(28.993, 44.664, 18.545), Angle(38.395, 0, 0) }
+SWEP.IronSightsPos 				= Vector(-5.921, -2.708, 2.599)
+SWEP.IronSightsAng 				= Angle(0.057, -0.029, 0)
+SWEP.SwayPosDiv					= 30
+SWEP.SwayAngDiv					= 15
+
+SWEP.UseIrons					= false
+SWEP.UseScope					= false
+SWEP.Zoom 						= 30
+SWEP.SetFATOnShoot 				= false
+
+SWEP.DTFloats 					= {}
+SWEP.DTBools 					= {}
+SWEP.DTInts 					= {}
+
+SWEP.ViewModelBoneMods = {}
+SWEP.VElements = {}
+SWEP.WElements = {}
