44cb4b750696c27f7f47db39a86706ed3a3d532f
Awcmon <adcarbine@yahoo.com>
2016-01-03 20:22:59 -0800

    Added CS:S AWP, SG552, fixed upside-down viewmodels

--- lua/weapons/weapon_wat_css_fiveseven/shared.lua
+++ lua/weapons/weapon_wat_css_fiveseven/shared.lua
@@ -65,7 +65,7 @@ SWEP.Primary.MuzzleEffects		= { "effect_wat_muzzle_flash", "effect_wat_muzzle_sm
 SWEP.RecoilPitchAdd 			= 1
 SWEP.RecoilPitchMul 			= 0.5
 SWEP.RecoilPitchMulAddMax		= 10
-SWEP.RecoilYawAdd 				= 3
+SWEP.RecoilYawAdd 				= 1
 SWEP.RecoilYawMul 				= 0.05
 
 SWEP.SpreadConeAdd 				= 0.005
